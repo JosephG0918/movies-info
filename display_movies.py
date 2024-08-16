e271733d230d8feb48fa8bee2001119f1a2ca6de
JosephG0918 <jkgossett@outlook.com>
2024-08-16 15:56:06 -0700

    first commit

---
+++ display_movies.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+from tabulate import tabulate
+
+try:
+    conn = sqlite3.connect("C:\\Users\\jkgos\\CodingWorkspace\\sqliteDB-saves\\movies.db")
+except sqlite3.Error as e:
+    print(f"Error connecting to SQLite3 database: {e}")
+
+df = pd.read_sql_query("SELECT date, movie_title, IMDb_rating, tomatometer, featured_user_review FROM amc_movies WHERE date = (SELECT MAX(date) FROM amc_movies);", conn)
+print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
